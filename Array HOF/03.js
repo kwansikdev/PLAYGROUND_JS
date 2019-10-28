8499c729e3746f4988d0b831b6c9c17c09af45ba
kwansikdev <kwansk0424@gmail.com>
2019-10-28 10:53:33 +0900

    Array HOF edited...

--- Array HOF/03.js
+++ Array HOF/03.js
@@ -6,6 +6,7 @@ const todos = [
   { id: 1, content: 'Javascript', completed: false }
 ];
 
+
 function sortBy(key) {
   // const results = todos.map(todo => todo);
   // const results = todos.slice();
@@ -16,26 +17,5 @@ function sortBy(key) {
 }
 
 console.log(sortBy('id'));
-/*
-[
-  { id: 1, content: 'Javascript', completed: false },
-  { id: 2, content: 'CSS', completed: true },
-  { id: 3, content: 'HTML', completed: false }
-]
-*/
 console.log(sortBy('content'));
-/*
-[
-  { id: 2, content: 'CSS', completed: true },
-  { id: 3, content: 'HTML', completed: false },
-  { id: 1, content: 'Javascript', completed: false }
-]
-*/
 console.log(sortBy('completed'));
-/*
-[
-  { id: 3, content: 'HTML', completed: false },
-  { id: 1, content: 'Javascript', completed: false },
-  { id: 2, content: 'CSS', completed: true }
-]
-*/
